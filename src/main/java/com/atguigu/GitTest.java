4c4f52914648e2bc9d03b2fad4c83d84509a3dd9
李闯 <1312933567@qq.com>
2021-01-19 01:11:26 +0800

    创建工程

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author ClamLee
+ * @create $(YEAR)-$(MONTH)-$(DAY)-$(TIME)
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("1111");
+    }
+}
